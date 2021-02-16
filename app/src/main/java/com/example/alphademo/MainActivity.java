034a2bc32b056c7e9b4fef6f8af46263f61c831b
suraj3poudel <suraj3poudel@gmail.com>
2021-02-16 17:42:42 -0600

    Test comment added

--- app/src/main/java/com/example/alphademo/MainActivity.java
+++ app/src/main/java/com/example/alphademo/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-
+        //view results
         btnview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
