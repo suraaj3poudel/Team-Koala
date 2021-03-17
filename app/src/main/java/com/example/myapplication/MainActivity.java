a4f171652a3e73d71e900a96bc85029f13c2c968
suraj3poudel <suraj3poudel@gmail.com>
2021-03-16 19:25:30 -0500

    New Upload Recycler View

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText username, password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        username = findViewById(R.id.id);
+        password = findViewById(R.id.password);
+
+        onClickLogin();
+
+    }
+
+
+    private void onClickLogin(){
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+
+                    openTripDetails();
+                }
+                else{
+                    showMessage("Message", "Error Logging In!");
+                }
+            }
+        });
+    }
+
+    private void openTripDetails() {
+        Intent intent = new Intent(this, TripDetails.class);
+        startActivity(intent);
+    }
+
+    private void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+
+        builder.setPositiveButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TripDetails.java
@@ -0,0 +1,82 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TripDetails extends AppCompatActivity {
+
+
+    RecyclerView recyclerView;
+    ArrayList<TripInfoClass> driverNames;
+    private static String JSON_URL = "https://jsonplaceholder.typicode.com/users";
+    RecyclerViewAdapter adapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_details);
+
+        recyclerView = findViewById(R.id.driverList);
+        driverNames = new ArrayList<TripInfoClass>();
+        extractDriverNames();
+
+
+
+    }
+
+    private void extractDriverNames() {
+        RequestQueue  queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject driverObject = response.getJSONObject(i);
+                        Toast.makeText(getApplicationContext(),"fetching all data",Toast.LENGTH_SHORT).show();
+                        TripInfoClass trip = new TripInfoClass();
+
+                        trip.setDriverName(driverObject.getString("name".toString()));
+
+                        driverNames.add(trip);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+                adapter = new RecyclerViewAdapter(getApplicationContext(), driverNames);
+
+                recyclerView.setAdapter(adapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TAG", "onErrorResponse: ");
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+    }
+}
