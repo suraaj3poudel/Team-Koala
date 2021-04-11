1e22c567d3d3f161b499625b1ae9d95221f2fd5b
sakhilabista <sakhilabista27@gmail.com>
2021-04-10 20:16:21 -0500

    added triplist

--- app/src/main/java/com/example/alphademo/TripListFragment.java
+++ app/src/main/java/com/example/alphademo/TripListFragment.java
@@ -1,98 +1,105 @@
-package com.example.alphademo;
+package com.example.alphademo.;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.alphademo.R;
+import com.example.alphademo.RecyclerViewAdapter;
+import com.example.alphademo.TripInfoClass;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TripListFragment extends Fragment {
 
-        RecyclerView recyclerView;
-        ArrayList<TripInfoClass> driverNames;
-        private static String JSON_URL = "https://api.appery.io/rest/1/apiexpress/api/DispatcherMobileApp/?apiKey=f20f8b25-b149-481c-9d2c-41aeb76246ef&Code=%7BCode%7D&Active=true";
-        RecyclerViewAdapter adapter;
+
+    RecyclerView recyclerView;
+    ArrayList<TripInfoClass> driverNames;
+    ProgressBar pbar;
+    private static String JSON_URL = "https://api.appery.io/rest/1/apiexpress/api/DispatcherMobileApp/GetTripListDetailByDriver/D1?apiKey=f20f8b25-b149-481c-9d2c-41aeb76246ef";
+    RecyclerViewAdapter adapter;
 
 
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.fragment_triplist);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_details);
 
-            recyclerView = recyclerView.findViewById();
-            driverNames = new ArrayList<TripInfoClass>();
-            extractDriverNames();
+        recyclerView = findViewById(R.id.driverList);
+        pbar = findViewById(R.id.progressBar2);
+        driverNames = new ArrayList<TripInfoClass>();
 
 
+        extractDriverNames();
+        onClickAddNotes();
 
-        }
 
-    private void setContentView(int fragment_triplist) {
+    }
+
+    private void onClickAddNotes() {
+
     }
 
     private void extractDriverNames() {
-            RequestQueue  queue = Volley.newRequestQueue(this);
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    for (int i = 0; i < response.length(); i++) {
-                        try {
-                            JSONObject driverObject = response.getJSONObject(i);
-                            Toast.makeText(getApplicationContext(),"fetching",Toast.LENGTH_SHORT).show();
-                            TripInfoClass trip = new TripInfoClass();
+        RequestQueue  queue = Volley.newRequestQueue(this);
 
-                            trip.setDriverName(driverObject.getString("data".toString()));
+        pbar.setVisibility(View.VISIBLE);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONObject>() {
 
-                            driverNames.add(trip);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
 
-                    recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            @Override
+            public void onResponse(JSONObject response) {
+                //Toast.makeText(getApplicationContext(),"fetching all data",Toast.LENGTH_SHORT).show();
+                //for (int i = 0; i < response.length(); i++) {
+                try {
 
-                    adapter = new RecyclerViewAdapter(getApplicationContext(), driverNames);
+                    JSONObject driverObject = response.getJSONObject("data".toString());
+                    JSONArray driverInfo = driverObject.getJSONArray("resultSet1".toString());
 
-                    recyclerView.setAdapter(adapter);
+                    TripInfoClass trip = new TripInfoClass(driverInfo);
+
+                    driverNames.add(trip) ;
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("TAG", "onErrorResponse: ");
-                }
-            });
+                //}
 
-            queue.add(jsonArrayRequest);
-        }
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
+                adapter = new RecyclerViewAdapter(getApplicationContext(), driverNames);
 
+                recyclerView.setAdapter(adapter);
 
+                pbar.setVisibility(View.VISIBLE);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TAG", "onErrorResponse: ");
+            }
+        });
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_triplist,container,false);
+        queue.add(jsonObjectRequest);
     }
 }
