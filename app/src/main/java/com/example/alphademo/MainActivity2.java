d2e043af055defe4703c9250e1a259ad1b7fcc5e
sakhilabista <sakhilabista27@gmail.com>
2021-04-10 20:06:53 -0500

    corrected bottom navigation

--- app/src/main/java/com/example/alphademo/MainActivity2.java
+++ app/src/main/java/com/example/alphademo/MainActivity2.java
@@ -1,22 +1,14 @@
 package com.example.alphademo;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity2 extends AppCompatActivity  {
 
@@ -29,7 +21,7 @@ public class MainActivity2 extends AppCompatActivity  {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelected);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TripFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Trip_listFragment()).commit();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelected = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,11 +31,11 @@ public class MainActivity2 extends AppCompatActivity  {
             Fragment select = null;
             switch (item.getItemId())
             {
-                case R.id.trip:
-                    select = new TripFragment();
+                case R.id.trip_list:
+                    select = new Trip_listFragment();
                     break;
-                case R.id.profile:
-                    select = new ProfileFragment();
+                case R.id.map:
+                    select = new MapFragment();
                     break;
                 case R.id.setting:
                     select = new SettingFragment();
