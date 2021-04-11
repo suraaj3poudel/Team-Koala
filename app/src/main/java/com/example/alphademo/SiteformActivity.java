73f57d3dc51404749c6a1a64300abe4dba419055
bijitaadhikari <vigitaadhikari@gmail.com>
2021-04-10 20:37:42 -0500

    Submission form for site

---
+++ app/src/main/java/com/example/alphademo/SiteformActivity.java
@@ -0,0 +1,14 @@
+package com.example.alphademo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SiteformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.siteform);
+    }
+}
