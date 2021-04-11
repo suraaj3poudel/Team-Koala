345eea1ae2b0615fbeb1e9d70d80596a0435161f
Ujjwal Parajuli <47229269+ujjwalparajuli@users.noreply.github.com>
2021-04-11 10:00:34 -0500

    Added datamodel class from alphademo to put all classes in one place

---
+++ datamodel.java
@@ -0,0 +1,30 @@
+package com.example.localdatabase;
+
+public class datamodel {
+
+    int image;
+    String header;
+
+    public datamodel(int image, String header) {
+        this.image = image;
+        this.header = header;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+}
+
+
