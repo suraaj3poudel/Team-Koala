9ef7a81b512b80c87bfa4d8e37c4aa939434ec2c
sakhilabista <sakhilabista27@gmail.com>
2021-04-12 15:36:54 -0500

    Changed Ui little bit

--- app/src/main/java/com/example/alphademo/SettingFragment.java
+++ app/src/main/java/com/example/alphademo/SettingFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +14,21 @@ import androidx.fragment.app.Fragment;
 
 public class SettingFragment extends Fragment {
 
+    ListView listView ;
+    SearchView searchview;
+    ArrayAdapter<String> adapter;
+    String[] data ={"Notification", "About", "Logout","Contact Dispatcher"};
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting,container,false);
+        View view = inflater.inflate(R.layout.fragment_setting,container,false);
+        listView = (ListView) view.findViewById(R.id.listview);
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.activity_list_item,data);
+        listView.setAdapter(adapter);
+        return view;
+
+
     }
 }
