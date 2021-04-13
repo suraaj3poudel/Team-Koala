e240e8390708b06d979134bfb16290e04b18f447
Ujjwal Parajuli <47229269+ujjwalparajuli@users.noreply.github.com>
2021-04-12 23:28:31 -0500

    localDatabase_Site

--- Site.java
+++ Site.java
@@ -3,21 +3,30 @@ package com.example.localdatabase;
 public class Site {
 
     // Attributes
-    String site = "";
-    String siteCode = "";
-    String siteAddress = "";
-    String siteCity = "";
-    String siteState = "";
-    String siteZIP = "";
+
+    String siteCode;
+    String siteName;
+    String siteAddress;
+    String siteCity;
+    String siteState;
+    String siteZIP;
 
     // Constructor Empty
     public Site() {
 
     }
 
-    // Constructors
-    public Site(String site, String siteCode, String siteAddress, String siteCity, String siteState, String siteZIP) {
-        this.site = site;
+    /**
+     *
+     * @param siteCode Code of the site
+     * @param siteName Name of the site
+     * @param siteAddress Address of the site
+     * @param siteCity City of the site
+     * @param siteState State of the site
+     * @param siteZIP ZIP code of the site
+     */
+    public Site(String siteName, String siteCode, String siteAddress, String siteCity, String siteState, String siteZIP) {
+        this.siteName = siteName;
         this.siteCode = siteCode;
         this.siteAddress = siteAddress;
         this.siteCity = siteCity;
@@ -25,21 +34,21 @@ public class Site {
         this.siteZIP = siteZIP;
     }
 
-    // Getters and Setters
-    public String getSite() {
-        return site;
-    }
-
-    public void setSite(String site) {
-        this.site = site;
-    }
-
+    //Getters and Setters
     public String getSiteCode() {
         return siteCode;
     }
 
     public void setSiteCode(String siteCode) {
         this.siteCode = siteCode;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
     }
 
     public String getSiteAddress() {
