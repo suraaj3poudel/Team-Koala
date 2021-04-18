acfebc83bce7832d1c7a14b00eb93f486083cc93
bijitaadhikari <vigitaadhikari@gmail.com>
2021-04-17 20:49:02 -0500

    camera access and image capture added

--- app/src/main/java/com/example/alphademo/dummy/MainActivity4.java
+++ app/src/main/java/com/example/alphademo/dummy/MainActivity4.java
@@ -1,14 +1,26 @@
 package com.example.alphademo.dummy;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.alphademo.R;
 import java.text.DateFormat;
@@ -16,10 +28,16 @@ import java.util.Calendar;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
     EditText date;
     DatePickerDialog datePickerDialog;
     TextView time1,time2;
     int t1hour,t2hour,t1min,t2min;
+
+    Button mCaptureBtn;
+    ImageView mImageView;
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,9 +97,9 @@ public class MainActivity4 extends AppCompatActivity {
         time2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar ncurrentTime = Calendar.getInstance();
-                int hour = ncurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = ncurrentTime.get(Calendar.MINUTE);
+                Calendar nCurrentTime = Calendar.getInstance();
+                int hour = nCurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = nCurrentTime.get(Calendar.MINUTE);
                 TimePickerDialog nTimePicker;
                 nTimePicker = new TimePickerDialog(MainActivity4.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
@@ -93,6 +111,85 @@ public class MainActivity4 extends AppCompatActivity {
             }
         });
 
+        mImageView = findViewById(R.id.image_view);
+        mCaptureBtn = findViewById(R.id.button5);
 
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if system os is >= marshmallow, request for run time permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED){
+                        //if permission not enabled, request for the permission
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+                        //popup msg to request permission
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+
+                    else {
+                        // if permission is already granted
+                        openCamera();
+                    }
+
+                    }
+                else {
+                    // system os < marshmallow
+                    openCamera();
+                }
+            }
+        });
+
+
+    }
+
+    private void openCamera() {
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+
+
+    }
+
+    // handles permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //method is called when user chooses allow or deny from permission request popup
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                //permission granted
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED){
+
+                    openCamera();
+                }
+                //permission denied
+                else {
+                    Toast.makeText(this, "Permission Denied...",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //called when image captured from camera
+        if (resultCode == RESULT_OK){
+
+            mImageView.setImageURI(image_uri);
+        }
     }
 }
