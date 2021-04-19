67c4cc2c00ca9de3b89a5ec03f4d54ffbddd0a32
sakhilabista <sakhilabista27@gmail.com>
2021-04-18 19:22:24 -0500

    Added gradient to the UI

--- app/src/main/java/com/example/alphademo/views/triplist/SourceListFragment.java
+++ app/src/main/java/com/example/alphademo/views/triplist/SourceListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,16 +19,25 @@ import com.example.alphademo.databinding.FragmentSourceListBinding;
 public class SourceListFragment extends Fragment {
     FragmentSourceListBinding binding;
     RecyclerViewSource adapter;
+   ImageButton img;
+
 
     public SourceListFragment(RecyclerViewSource sourceAdapter) {
         adapter = sourceAdapter;
+
     }
+
+
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_source_list,container,false);
         binding.sourceList.setAdapter(adapter);
+
         return binding.getRoot();
     }
 }
