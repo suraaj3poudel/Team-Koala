df461a497728821bbc89ec0f35e14de4a905df3d
DipeshTami <dipesh.sht15@gmail.com>
2021-04-20 02:57:32 -0500

    Database and profile added

--- app/src/main/java/com/example/alphademo/views/setting/SettingFragment.java
+++ app/src/main/java/com/example/alphademo/views/setting/SettingFragment.java
@@ -3,9 +3,13 @@ package com.example.alphademo.views.setting;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +19,14 @@ import com.example.alphademo.MainActivity;
 import com.example.alphademo.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class SettingFragment extends Fragment
 {
     private Button button;
     private Button logoutBtn;
+    private ImageView setting;
 
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
@@ -27,7 +35,7 @@ public class SettingFragment extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
-        button = (Button) view.findViewById(R.id.about_btn);
+       /* button = (Button) view.findViewById(R.id.about_btn);
         logoutBtn=(Button)view.findViewById(R.id.logout_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,8 +51,72 @@ public class SettingFragment extends Fragment
                 startActivity(intent);
 
             }
-        });
+        });*/
+
+
+
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setting = (ImageView)getView().findViewById(R.id.settingIcon);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenu(v);
+
+            }
+        });
+
+    }
+
+    private void showMenu(View v){
+        PopupMenu popupMenu = new PopupMenu(getContext(), v);
+        popupMenu.getMenuInflater().inflate(R.menu.menu_settings,popupMenu.getMenu());
+        Boolean is = true;
+
+
+
+
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.logout){
+                    Toast.makeText(getContext(), "Pressed Log Out Button", Toast.LENGTH_SHORT).show();
+                }
+                if(item.getItemId() == R.id.editProfile){
+                    Toast.makeText(getContext(), "Pressed Edit Profile Button", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+        if(is){
+            try {
+                Field[] fields = popupMenu.getClass().getDeclaredFields();
+                for (Field field : fields) {
+                    if ("mPopup".equals(field.getName())) {
+                        field.setAccessible(true);
+                        Object menuPopupHelper = field.get(popupMenu);
+                        Class<?> classPopupHelper = Class.forName(menuPopupHelper.getClass().getName());
+                        Method setForceIcons = classPopupHelper.getMethod("setForceShowIcon", boolean.class);
+                        setForceIcons.invoke(menuPopupHelper, true);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+
+        popupMenu.show();
+
     }
     public void openDialog(){
         DialogFragment dialog =new DialogFragment();
