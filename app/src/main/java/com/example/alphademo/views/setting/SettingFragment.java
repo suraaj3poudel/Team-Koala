538cf1f8db379fc9b20717e23ff93fb755eb4590
DipeshTami <dipesh.sht15@gmail.com>
2021-04-27 04:12:56 -0500

    Settings Updated

--- app/src/main/java/com/example/alphademo/views/setting/SettingFragment.java
+++ app/src/main/java/com/example/alphademo/views/setting/SettingFragment.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,8 +30,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-public class SettingFragment extends Fragment
-{
+public class SettingFragment extends Fragment {
     private Button button;
     private Button logoutBtn;
     private ImageView setting;
@@ -38,10 +38,12 @@ public class SettingFragment extends Fragment
     EditText editemail, editPhone, editAddress, editName;
     RadioGroup radioGender;
     RadioButton radiobtn;
-    TextView textEmail, textPhone, textAddress, textGender, textName;
+    TextView textEmail, textPhone, textAddress, textGender, textName, tv_address, displayName;
     String name, email, address, gender, phone;
     String updateName, updateEmail, updatePhone, updateAddress, updateGender;
+    ImageView imageEmail, imagePhone, imageAddress, imageGender, imageName;
     Button save;
+    RelativeLayout topSection;
 
 
     FirebaseAuth mAuth;
@@ -70,7 +72,6 @@ public class SettingFragment extends Fragment
         });*/
 
 
-
         return view;
     }
 
@@ -78,32 +79,39 @@ public class SettingFragment extends Fragment
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Log.d("TAG", "onErrorResponse1: ");
-        setting = (ImageView)getView().findViewById(R.id.settingIcon);
+        setting = (ImageView) getView().findViewById(R.id.settingIcon);
         editemail = (EditText) getView().findViewById(R.id.edittextemail);
         editPhone = (EditText) getView().findViewById(R.id.editPhone);
-        editAddress= (EditText) getView().findViewById(R.id.editAddress);
+        editAddress = (EditText) getView().findViewById(R.id.editAddress);
         editName = (EditText) getView().findViewById(R.id.editName);
 
         radioGender = (RadioGroup) getView().findViewById(R.id.radioGrp);
+        imageEmail = (ImageView) getView().findViewById(R.id.imageEmail);
+        imagePhone = (ImageView) getView().findViewById(R.id.imagePhone);
+        imageAddress = (ImageView) getView().findViewById(R.id.imageAddress);
+        imageGender = (ImageView) getView().findViewById(R.id.imageGender);
+        imageName = (ImageView) getView().findViewById(R.id.imageName);
 
         textEmail = (TextView) getView().findViewById(R.id.textviewemail);
         textPhone = (TextView) getView().findViewById(R.id.textPhone);
         textAddress = (TextView) getView().findViewById(R.id.textAddress);
         textGender = (TextView) getView().findViewById(R.id.textGender);
         textName = (TextView) getView().findViewById(R.id.textName);
+        tv_address = (TextView) getView().findViewById(R.id.tv_address);
+        displayName = (TextView) getView().findViewById(R.id.displayName);
+
+        topSection = getView().findViewById(R.id.topSection);
+
         phone = textPhone.getText().toString();
 
         save = (Button) getView().findViewById((R.id.saveButton));
 
 
-
-
-
-         db =  new DatabaseProfile(getContext());
-         //db.addData(1, "jhhjhj", "", "", "","");
+        db = new DatabaseProfile(getContext());
+        //db.addData(1, "jhhjhj", "", "", "","");
         //name = "ABCDEFGH";
         //db.getData(2, "EMAIL");
-        Toast.makeText(getContext(),db.getData(1, "NAME").toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), db.getData(1, "NAME").toString(), Toast.LENGTH_SHORT).show();
 
 
 
@@ -120,15 +128,24 @@ public class SettingFragment extends Fragment
         textAddress.setText(db.getData(1, "ADDRESS"));
         textPhone.setText(db.getData(1, "PHONE"));
         textGender.setText(db.getData(1, "GENDER"));
+        tv_address.setText(db.getData(1, "ADDRESS"));
+        displayName.setText(db.getData(1, "NAME"));
 
 
+        editName.setText(db.getData(1, "NAME"));
+        editemail.setText(db.getData(1, "EMAIL"));
+        editPhone.setText(db.getData(1, "PHONE"));
+        editAddress.setText(db.getData(1, "ADDRESS"));
+
+
+
+        if (db.getData(1, "GENDER").toString().equals("Female"))
+            radioGender.check(R.id.radioF);
+        else
+            radioGender.check(R.id.radioM);
 
 
         //Toast.makeText(getContext(),db.getData(1, "EMAIL").toString(), Toast.LENGTH_SHORT).show();
-
-
-
-
 
 
         setting.setOnClickListener(new View.OnClickListener() {
@@ -142,38 +159,68 @@ public class SettingFragment extends Fragment
 
     }
 
-    private void showMenu(View v){
+    private void showMenu(View v) {
         PopupMenu popupMenu = new PopupMenu(getContext(), v);
-        popupMenu.getMenuInflater().inflate(R.menu.menu_settings,popupMenu.getMenu());
+        popupMenu.getMenuInflater().inflate(R.menu.menu_settings, popupMenu.getMenu());
         Boolean is = true;
-
-
-
 
 
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                if(item.getItemId() == R.id.logout){
+                if (item.getItemId() == R.id.logout) {
                     Toast.makeText(getContext(), "Pressed Log Out Button", Toast.LENGTH_SHORT).show();
                 }
-                if(item.getItemId() == R.id.editProfile){
-                    editemail.setVisibility(View.VISIBLE);
-                    editPhone.setVisibility(View.VISIBLE);
-                    editAddress.setVisibility(View.VISIBLE);
-                    editName.setVisibility(View.VISIBLE);
-                    radioGender.setVisibility(View.VISIBLE);
+                if (item.getItemId() == R.id.editProfile) {
+
+
                     save.setVisibility(View.VISIBLE);
+                    imageEmail.setVisibility(View.VISIBLE);
+                    imageAddress.setVisibility(View.VISIBLE);
+                    imageGender.setVisibility(View.VISIBLE);
+                    imagePhone.setVisibility(View.VISIBLE);
+                    imageName.setVisibility(View.VISIBLE);
+
+                    topSection.setVisibility(View.GONE);
 
 
-                    textEmail.setVisibility(View.GONE);
-                    textPhone.setVisibility(View.GONE);
-                    textAddress.setVisibility(View.GONE);
-                    textGender.setVisibility(View.GONE);
-                    textName.setVisibility(View.GONE);
 
-
-                    Toast.makeText(getContext(), "Pressed Edit Profile Button", Toast.LENGTH_SHORT).show();
+                    imageEmail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            appears(editemail, textEmail, imageEmail, "EMAIL");
+                        }
+                    });
+                    imagePhone.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            appears(editPhone, textPhone, imagePhone, "PHONE");
+                        }
+                    });
+                    imageAddress.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            appears(editAddress, textAddress, imageAddress, "ADDRESS");
+                        }
+                    });
+                    imageName.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            appears(editName, textName, imageName, "NAME");
+                        }
+                    });
+                    imageGender.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            imageGender.setVisibility(View.GONE);
+                            radioGender.setVisibility(View.VISIBLE);
+                            textGender.setVisibility(View.GONE);
+                            if (db.getData(1, "GENDER").toString().equals("Female"))
+                                radioGender.check(R.id.radioF);
+                            else
+                                radioGender.check(R.id.radioM);
+                        }
+                    });
 
                     save.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -185,7 +232,7 @@ public class SettingFragment extends Fragment
                 return false;
             }
         });
-        if(is){
+        if (is) {
             try {
                 Field[] fields = popupMenu.getClass().getDeclaredFields();
                 for (Field field : fields) {
@@ -206,15 +253,16 @@ public class SettingFragment extends Fragment
         }
 
 
-
         popupMenu.show();
 
     }
-    public void openDialog(){
-        DialogFragment dialog =new DialogFragment();
-        dialog.show(getChildFragmentManager(),"example ");
+
+    public void openDialog() {
+        DialogFragment dialog = new DialogFragment();
+        dialog.show(getChildFragmentManager(), "example ");
     }
-    public void saveFunction(String updateAddress1){
+
+    public void saveFunction(String updateAddress1) {
         updateAddress = editAddress.getText().toString();
         updateName = editName.getText().toString();
         updateEmail = editemail.getText().toString();
@@ -222,14 +270,16 @@ public class SettingFragment extends Fragment
         int selectedId = radioGender.getCheckedRadioButtonId();
         radiobtn = (RadioButton) getView().findViewById(selectedId);
         updateGender = radiobtn.getText().toString();
-        db.addData(1, updateName, updateAddress,  updateEmail, updatePhone, updateGender);
-        db.updateInfo(1, updateName, updateAddress,  updateEmail, updatePhone, updateGender);
+        db.addData(1, updateName, updateAddress, updateEmail, updatePhone, updateGender);
+        db.updateInfo(1, updateName, updateAddress, updateEmail, updatePhone, updateGender);
 
         textPhone.setText(updatePhone);
         textEmail.setText(updateEmail);
         textAddress.setText(updateAddress);
         textGender.setText(updateGender);
         textName.setText(updateName);
+        tv_address.setText(updateAddress);
+        displayName.setText(updateName);
 
         editemail.setVisibility(View.GONE);
         editPhone.setVisibility(View.GONE);
@@ -238,18 +288,32 @@ public class SettingFragment extends Fragment
         editName.setVisibility(View.GONE);
         save.setVisibility(View.GONE);
 
+        imageEmail.setVisibility(View.GONE);
+        imageAddress.setVisibility(View.GONE);
+        imageGender.setVisibility(View.GONE);
+        imagePhone.setVisibility(View.GONE);
+        imageName.setVisibility(View.GONE);
+
         textEmail.setVisibility(View.VISIBLE);
         textPhone.setVisibility(View.VISIBLE);
         textAddress.setVisibility(View.VISIBLE);
         textGender.setVisibility(View.VISIBLE);
         textName.setVisibility(View.VISIBLE);
-
-
-
+        tv_address.setVisibility(View.VISIBLE);
+        topSection.setVisibility(View.VISIBLE);
 
 
     }
+
+    public void appears(EditText appear, TextView disappear, ImageView image, String field) {
+        appear.setVisibility(View.VISIBLE);
+        appear.setText(db.getData(1, field));
+        disappear.setVisibility(View.GONE);
+        image.setVisibility(View.GONE);
+    }
 }
+
+
 /*
 package com.example.alphademo.views.setting;
 
